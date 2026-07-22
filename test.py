c7a2e074a2dee2f1bbb4366c4b954013f41e63bc
Do Nhat Nam <nnam@Dos-MacBook-Pro-2.local>
2026-07-22 22:01:36 +0700

    fea 2  commit

--- test.py
+++ test.py
@@ -3,10 +3,8 @@ def add(a, b):
     print(f'Adding two numbers with a={a}, b={b}')
     return a+b
 
-def add(a, b):
-    print('Adding two numbers a and b')
-    print(f'Adding two numbers with a={a}, b={b}')
-    return a+b
+def multiply(a, b):
+    return a*b
 
 def add(a, b):
     print('Adding two numbers a and b')
