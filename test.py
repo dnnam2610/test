af6cbd010fca8f1e8f657e51bdfbdd60ede316bc
Do Nhat Nam <nnam@Dos-MacBook-Pro-2.local>
2026-07-22 21:37:41 +0700

    main  commit

--- test.py
+++ test.py
@@ -2,3 +2,18 @@ def add(a, b):
     print('Adding two numbers a and b')
     print(f'Adding two numbers with a={a}, b={b}')
     return a+b
+
+def add(a, b):
+    print('Adding two numbers a and b')
+    print(f'Adding two numbers with a={a}, b={b}')
+    return a+b
+
+def add(a, b):
+    print('Adding two numbers a and b')
+    print(f'Adding two numbers with a={a}, b={b}')
+    return a+b
+
+def add(a, b):
+    print('Adding two numbers a and b')
+    print(f'Adding two numbers with a={a}, b={b}')
+    return a+b
